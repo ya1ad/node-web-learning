0e3f441751772d812ccf886d3c1fd58cda6009a7
yasir <aff3ad@gmail.com>
2018-03-19 18:06:56 +0200

    Modification for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
-const port = process.env.PROT || 3000
+const port = process.env.PROT
 var app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
