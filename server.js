49086b850d1998b95b08422184009e0694576a90
yasir <aff3ad@gmail.com>
2018-03-19 17:56:01 +0200

    Setup for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
+const port = process.env.PROT || 3000
 var app = express()
-
 
 hbs.registerPartials(__dirname + '/views/partials')
 
@@ -48,6 +48,6 @@ app.get('/bad', (req, res) => {
     errorMessage: 'Unable to handle request'
   })
 })
-app.listen(3000, () => {
-  console.log('Server is up on 3000')
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`)
 })
